0b633d473fc8a16b691ca659c5914acd763ed7a3
Stevan <stevanekill@gmail.com>
2023-07-13 22:38:09 -0300

    commit add POST FILTER, GET, PUT, DELETE and Configuration Class

--- TCC Java/V1.0/Coneccar V1.0/Coneccar/src/main/java/com/api/Coneccar/configuration/DateConfig.java
+++ TCC Java/V1.0/Coneccar V1.0/Coneccar/src/main/java/com/api/Coneccar/configuration/DateConfig.java
@@ -1,4 +1,4 @@
-package com.api.Coneccar.Configuration;
+package com.api.Coneccar.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
